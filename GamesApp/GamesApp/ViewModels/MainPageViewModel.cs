02857f77aec2909ccbaf8dffc41577759e3cd347
simasbakus <simas.bakus@gmail.com>
2020-11-27 09:27:18 +0200

    Initial commit

---
+++ GamesApp/GamesApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamesApp.ViewModels
+{
+    class MainPageViewModel
+    {
+        public string name { get; set; }
+
+        public MainPageViewModel()
+        {
+            this.name = "Simas Bakus";
+        }
+    }
+}
